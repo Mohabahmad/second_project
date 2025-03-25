bb195bdeac509471f4fb0ddcd60801f720a9c5e5
mohabahmad <mohabmm666@gmail.com>
2025-03-25 22:27:36 +0530

    new

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { BsFillPlayFill,BsPauseFill } from 'react-icons/bs';
+import {meal} from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo ,setplayVideo] = useState(false)
+  const vidFef = React.useRef();
+
+  const handleVideo = () => {
+    setplayVideo((prevPlayVideo) => !prevPlayVideo);
+    if(playVideo){
+      vidFef.current.pause();
+  }
+  else{
+    vidFef.current.play();
+  }
+}
+    return (
+    <div className='app__video'>
+   <video
+    src={meal} 
+    ref={vidFef}
+    type="video/mp4"
+    loop
+    controls={false}
+    muted
+    />
+    <div className='app__video-overlay flex-center'>
+      <div className='app__video-overlay_circle flex-center' onClick={handleVideo}>
+
+      </div>
+
+    </div>
+    
+  </div>   
+    )
+    
+  }
 
 export default Intro;
